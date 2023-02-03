4c83f0b9aacdd133bcac21f159f39f04f17439e7
Juan CV <jacklazerjscv@gmail.com>
2023-02-02 23:19:02 -0500

    controlador (faltan pequeños detalles)

--- Opcional/src/controlador/ValoracionDeInventarioControlador.java
+++ Opcional/src/controlador/ValoracionDeInventarioControlador.java
@@ -36,4 +36,86 @@ public class ValoracionDeInventarioControlador {
         listaValoracionDelInventario.put((valoracionDelInventario.getProducto()).getId(), valoracionDelInventario);
     }
 
+    public String verValoracionDelInventario(Integer id) {
+        String lista = "---------- Valoracion del inventario del producto " + ((listaValoracionDelInventario.get(id)).getProducto()).getNombre() + " ----------\n";
+        for(int idAux : listaValoracionDelInventario.keySet()){
+            if (idAux == id) {
+                lista += listaValoracionDelInventario.get(idAux) + "\n";
+            }
+        }
+        return lista;
+    }
+    
+    public void generarCSV() {
+        String archivoPersistencia = "";
+        for(int codigo : listaValoracionDelInventario.keySet()){
+            archivoPersistencia += listaValoracionDelInventario.get(codigo) + "\n";
+        }
+        try {
+            FileOutputStream os = new FileOutputStream(new File("src\\main\\java\\persistencia\\valoracionDeInventarioPersistencia.txt"));
+            os.write(archivoPersistencia.getBytes());
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(ValoracionDeInventarioControlador.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(ValoracionDeInventarioControlador.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void restaurarDatos() {
+        File archivo = new File("src\\main\\java\\persistencia\\valoracionDeInventarioPersistencia.txt");
+        StringTokenizer stringTokenizer;
+  
+        String cadena1;
+        String cadena2;
+        
+        Integer id;
+        String nombre;
+        
+        Integer cantidad;
+        double valorUnidad;
+        double valorTotal;
+        
+        try {
+            FileReader fileReader = new FileReader(archivo);
+            try (BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+
+                while ((cadena1 = bufferedReader.readLine()) != null){
+                    cadena2 = cadena1.replace("{", "");
+                    cadena1 = cadena2.replace("Id: ", "");
+                    cadena2 = cadena1.replace(" Nombre: ", "");
+                    cadena1 = cadena2.replace(" Cantidad: ", "");
+                    cadena2 = cadena1.replace(" Valor unidad: ", "");
+                    cadena1 = cadena2.replace(" Valor total: ", "");
+                    cadena2 = cadena1.replace("}", "");
+                    
+                    stringTokenizer = new StringTokenizer(cadena2,",");
+                    
+                    if (stringTokenizer.countTokens() == 5) {
+                        id = Integer.valueOf(stringTokenizer.nextToken());
+                        nombre = stringTokenizer.nextToken();
+                        ProductoModelo producto = new ProductoModelo(id, nombre);
+                        
+                        cantidad = Integer.valueOf(stringTokenizer.nextToken());
+                        valorUnidad = Double.valueOf(stringTokenizer.nextToken());
+                        valorTotal = Double.valueOf(stringTokenizer.nextToken());
+                        
+                        ValoracionDelInventarioModelo valoracionDelInventario = new ValoracionDelInventarioModelo(producto, cantidad, valorUnidad, valorTotal);
+                        listaValoracionDelInventario.put(id, valoracionDelInventario);
+                    } 
+                }
+            }
+        }
+         catch (FileNotFoundException ex) {
+            Logger.getLogger(MovimientosControlador.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(MovimientosControlador.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void valorizarElInventario(Integer id){
+        (listaValoracionDelInventario.get(id)).setCantidad(0);
+        (listaValoracionDelInventario.get(id)).setValorUnidad(0);
+        (listaValoracionDelInventario.get(id)).setValorTotal(0);
+        
+    }
 }
